a999975482192208250f80b565d1c78133d2b3de
AbuBakarAnsari4 <abubakaransari5625@gmail.com>
2023-10-24 13:46:21 -0700

    second

---
+++ ajax-live-search.php
@@ -0,0 +1,58 @@
+<?php
+
+$search_value = $_POST["search"];
+
+$conn = mysqli_connect("localhost","root","","test") or die("Connection Failed");
+
+$limit_per_page = 5;
+
+$page="";
+if(isset( $_POST["page_no"] )){
+    $page = $_POST["page_no"];
+}else{
+    $page = 1;
+}
+
+$offset = ($page -1) * $limit_per_page;
+
+$sql = "SELECT * FROM student WHERE first_name LIKE '%{$search_value}%' OR last_name LIKE '%{$search_value}%' LIMIT {$offset},{$limit_per_page}";
+
+$result = mysqli_query($conn, $sql) or die("SQL Query Failed.");
+$output = "";
+if(mysqli_num_rows($result) > 0 ){
+  $output = '<table border="1" width="100%" cellspacing="0" cellpadding="10px">
+              <tr>
+                <th width="60px">Id</th>
+                <th>Name</th>
+                <th width="90px">Edit</th>
+                <th width="90px">Delete</th>
+              </tr>';
+
+              while($row = mysqli_fetch_assoc($result)){
+                $output .= "<tr><td align='center'>{$row["id"]}</td><td>{$row["first_name"]} {$row["last_name"]}</td><td align='center'><button class='edit-btn' data-eid='{$row["id"]}'>Edit</button></td><td align='center'><button Class='delete-btn' data-id='{$row["id"]}'>Delete</button></td></tr>";
+              }
+    $output .= "</table>";
+
+    $sql_total = "SELECT * FROM student WHERE first_name LIKE '%{$search_value}%' OR last_name LIKE '%{$search_value}%'";
+    $records = mysqli_query( $conn, $sql_total) or die("Query FAiled");
+    $total_record  = mysqli_num_rows($records);
+    $total_pages = ceil($total_record/$limit_per_page);
+
+    $output .='<div id="pagination">';
+
+    for($i=1;$i <= $total_pages; $i++){
+        if($i == $page){
+            $class_name = "active";
+        }else{
+            $class_name = "";
+        }
+        $output .="<a class='{$class_name}' id='{$i}' href=''>{$i}</a>";
+    }
+    $output .='</div>';
+echo $output;
+}else{
+echo "<h2>No Record Found.</h2>";
+}
+
+
+?>
